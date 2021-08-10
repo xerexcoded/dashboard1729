7ba89ae645d4870d13598884f7bf7d4466568654
xerexcoded <arnavbanerjee61@gmail.com>
2021-08-11 00:45:10 +0530

    ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import Dashboard from './dashboard'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+      <Dashboard/>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
